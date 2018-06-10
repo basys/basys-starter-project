d9caa930e4d59088e23508caf880624e2475e467
Sergey <sergkop@gmail.com>
2018-06-11 02:28:28 +0300

    Add explanations for backend.js and web.js

---
+++ src/backend.js
@@ -0,0 +1,2 @@
+// Add Express configuration and backend routes here.
+// For more details see https://basys.io/docs/app-code#backend-app .
---
+++ src/web.js
@@ -0,0 +1,2 @@
+// Here you can register Vue plugins and import front-end libraries.
+// For more details see https://basys.io/docs/app-code#front-end-entry .
